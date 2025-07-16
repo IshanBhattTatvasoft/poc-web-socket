6a28436de38ec7a8354cdcd3e0e2af500bab9ea7
Ishan Bhatt <ishan.bhatt@tatvasoft.com>
2025-07-16 18:37:33 +0530

    web socket code for v1

---
+++ src/webSocketServer.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws: WebSocket) => {
+
+  // Send the fixed message on connection
+  ws.send('Response from v1');
+});
+
